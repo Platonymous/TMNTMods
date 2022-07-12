7b50fe8b0a97e799d2957faf5751b9ff16e0a25e
Platonymous <david@millionideas.de>
2022-07-12 04:08:19 +0200

    Add config to SkipIntro

---
+++ src/SkipIntro/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace SkipIntro
+{
+    public class Config
+    {
+        public bool Logos { get; set; } = true;
+
+        public bool HowTo { get; set; } = true;
+
+        public bool Video { get; set; } = true;
+    }
+}
--- src/SkipIntro/SkipIntro.cs
+++ src/SkipIntro/SkipIntro.cs
@@ -1,33 +1,43 @@
 ﻿using Microsoft.Xna.Framework;
 using ModLoader;
 using Paris.Engine.Context;
+using Paris.Engine.Cutscenes;
 using Paris.Engine.Scene;
-using Paris.Engine.Scene.HUD;
-using Paris.Game.Data;
 using Paris.Game.HUD;
-using System.Reflection;
 
 namespace SkipIntro
 {
     public class SkipIntroMod : IMod
     {
-        static IModHelper Helper;
+        IModHelper Helper;
+        Config ModConfig;
 
         public void ModEntry(IModHelper helper)
         {
             Helper = helper;
+            ModConfig = helper.Content.LoadJson<Config>("config.json", new Config(), true);
+            
             helper.Events.ContextSwitched += Events_ContextSwitched;
         }
 
         private void Events_ContextSwitched(object sender, ModLoader.Events.ContextSwitchedEventArgs e)
         {
-            if (e.NewContext.Contains("Paris.Game.Menu.LogoScreen"))
+            if (ModConfig.Logos && e.NewContext.Contains("Paris.Game.Menu.LogoScreen"))
+            {
+                e.SwitchContext(ModConfig.Video ? "Paris.Game.Menu.MainMenu" : "Paris.Engine.Cutscenes.VideoContext");
+                if (!ModConfig.Video)
+                {
+                    VideoContext.VideoPath = "Videos\\TMNT_GameIntro";
+                    VideoContext.NextContext = "Paris.Game.Menu.MainMenu";
+                }
+            }
+            else if (ModConfig.Video && e.NewContext.Contains("Paris.Engine.Cutscenes.VideoContext"))
                 e.SwitchContext("Paris.Game.Menu.MainMenu");
-            else if (e.NewContext.Equals(@"2d\Level\Scene2d\HowToPlay", System.StringComparison.OrdinalIgnoreCase))
+            
+            if (ModConfig.HowTo && e.NewContext.Equals(@"2d\Level\Scene2d\HowToPlay", System.StringComparison.OrdinalIgnoreCase))
                 Helper.Events.UpdateTicked += Events_UpdateTicked1;
             else
                 Helper.Events.UpdateTicked -= Events_UpdateTicked1;
-
         }
 
         private void Events_UpdateTicked1(object sender, ModLoader.Events.UpdateTickEventArgs e)
